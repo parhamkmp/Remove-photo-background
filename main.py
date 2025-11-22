a5e95f1b4adb31079d94ac1c3f8d5bdc02278230
Parham Karimipour <parhamkarimipoor1386@gmail.com>
2025-11-23 01:01:11 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+from rembg import remove
+
+input_folder = "Incoming Photos Folder"
+output_folder = "Output photo folder"
+
+os.makedirs(output_folder, exist_ok=True)
+
+for file in os.listdir(input_folder):
+    if file.lower().endswith((".jpg", ".jpeg", ".png")):
+        with open(os.path.join(input_folder, file), "rb") as inp:
+            with open(os.path.join(output_folder, file + ".png"), "wb") as out:
+                out.write(remove(inp.read()))
